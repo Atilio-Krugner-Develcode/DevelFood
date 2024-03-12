f6d36bc71ede3d30c10da9cb4fb89f0d31bdd6f2
atilioleite <atilio.leite@develcode.com.br>
2024-03-12 09:19:19 -0300

    commit inicial

---
+++ src/main/java/br/com/develfood/develfood/DevelfoodApplication.java
@@ -0,0 +1,13 @@
+package br.com.develfood.develfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevelfoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/develfood/develfood/DevelfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.develfood.develfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevelfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
