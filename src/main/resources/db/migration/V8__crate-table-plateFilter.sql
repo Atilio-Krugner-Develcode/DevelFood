8d246143d9918728f0c04eb4dd0e8e13642dc910
atilioleite <atilio.leite@develcode.com.br>
2024-05-08 09:53:44 -0300

    Foi corrigido o bug de redefinir senha

--- src/main/resources/db/migration/V8__crate-table-plateFilter.sql
+++ src/main/resources/db/migration/V8__crate-table-plateFilter.sql
@@ -2,7 +2,15 @@ CREATE TABLE plate_filter (
     id SERIAL PRIMARY KEY NOT NULL,
     nome VARCHAR(255),
     tiposComidaId INTEGER,
+    tiposId INTEGER,
     restaurantId INTEGER,
     FOREIGN KEY (tiposComidaId) REFERENCES pratos(id),
-    FOREIGN KEY (restaurantId) REFERENCES restaurant(id)
+    FOREIGN KEY (restaurantId) REFERENCES restaurant(id),
+    FOREIGN KEY (tiposId) REFERENCES tiposComida(id)
+
 );
+
+INSERT INTO plate_filter (nome, tiposId) VALUES
+    ('Prato Italiano 1', 1),
+    ('Prato Japonês 1', 2),
+    ('Prato Mexicano 1', 3);
